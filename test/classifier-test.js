0ebe559159a7f2dc27559344366175baf7773127
Fergie McDowall <fergusmcdowall@gmail.com>
2017-07-21 14:30:07 +0200

    fix(classifier): fixed classification bug

--- test/classifier-test.js
+++ test/classifier-test.js
@@ -147,184 +147,184 @@ test('classify', function (t) {
     })
 })
 
-// test('classify maxNGramLength is 3', function (t) {
-//   var expectedResults = [
-//     { token: 'this', documents: [ '10', '2', '7' ] },
-//     { token: 'is', documents: [ '4', '8' ] },
-//     { token: 'is a', documents: [ '4' ] },
-//     { token: 'a', documents: [ '4', '6', '7' ] },
-//     { token: 'swiss', documents: [ '10', '2', '3', '4', '5', '9' ] },
-//     { token: 'and', documents: [ '1', '10', '7' ] },
-//     { token: 'a', documents: [ '4', '6', '7' ] },
-//     { token: 'watch', documents: [ '1', '10', '2', '3', '7', '9' ] },
-//     { token: 'watch from', documents: [ '7' ] },
-//     { token: 'watch from tw', documents: [ '7' ] },
-//     { token: 'from', documents: [ '5', '6', '7', '8' ] },
-//     { token: 'from tw', documents: [ '7' ] },
-//     { token: 'tw', documents: [ '7' ] }
-//   ]
-//   var s = new Readable()
-//   'This is a really interesting sentence about swiss watches and also a watch from tw wooo'
-//     .split(' ')
-//     .forEach(function (item) {
-//       s.push(item)
-//     })
-//   s.push(null)
-//   t.plan(14)
-//   s.pipe(sis.classify({
-//     maxNGramLength: 3
-//   }))
-//     .on('data', function (data) {
-//       t.looseEquals(data, expectedResults.shift())
-//     })
-//     .on('error', function (err) {
-//       t.error(err)
-//     })
-//     .on('end', function () {
-//       t.equals(expectedResults.length, 0)
-//     })
-// })
+test('classify maxNGramLength is 3', function (t) {
+  var expectedResults = [
+    { token: 'this', documents: [ '10', '2', '7' ] },
+    { token: 'is', documents: [ '4', '8' ] },
+    { token: 'is a', documents: [ '4' ] },
+    { token: 'a', documents: [ '4', '6', '7' ] },
+    { token: 'swiss', documents: [ '10', '2', '3', '4', '5', '9' ] },
+    { token: 'and', documents: [ '1', '10', '7' ] },
+    { token: 'a', documents: [ '4', '6', '7' ] },
+    { token: 'watch', documents: [ '1', '10', '2', '3', '7', '9' ] },
+    { token: 'watch from', documents: [ '7' ] },
+    { token: 'watch from tw', documents: [ '7' ] },
+    { token: 'from', documents: [ '5', '6', '7', '8' ] },
+    { token: 'from tw', documents: [ '7' ] },
+    { token: 'tw', documents: [ '7' ] }
+  ]
+  var s = new Readable()
+  'This is a really interesting sentence about swiss watches and also a watch from tw wooo'
+    .split(' ')
+    .forEach(function (item) {
+      s.push(item)
+    })
+  s.push(null)
+  t.plan(14)
+  s.pipe(sis.classify({
+    maxNGramLength: 3
+  }))
+    .on('data', function (data) {
+      t.looseEquals(data, expectedResults.shift())
+    })
+    .on('error', function (err) {
+      t.error(err)
+    })
+    .on('end', function () {
+      t.equals(expectedResults.length, 0)
+    })
+})
 
-// test('classify maxNGramLength is 3', function (t) {
-//   var expectedResults = [
-//     { token: 'endlessly', documents: [ '10' ] },
-//     { token: 'endlessly sophisticated', documents: [ '10' ] },
-//     { token: 'sophisticated', documents: [ '10' ] },
-//     { token: 'this', documents: [ '10', '2', '7' ] },
-//     { token: 'is', documents: [ '4', '8' ] },
-//     { token: 'is a', documents: [ '4' ] },
-//     { token: 'a', documents: [ '4', '6', '7' ] },
-//     { token: 'swiss', documents: [ '10', '2', '3', '4', '5', '9' ] },
-//     { token: 'and', documents: [ '1', '10', '7' ] },
-//     { token: 'a', documents: [ '4', '6', '7' ] },
-//     { token: 'watch', documents: [ '1', '10', '2', '3', '7', '9' ] },
-//     { token: 'watch from', documents: [ '7' ] },
-//     { token: 'watch from tw', documents: [ '7' ] },
-//     { token: 'from', documents: [ '5', '6', '7', '8' ] },
-//     { token: 'from tw', documents: [ '7' ] },
-//     { token: 'tw', documents: [ '7' ] }
-//   ]
-//   var s = new Readable()
-//   'Endlessly sophisticated This is a really interesting sentence about swiss watches and also a watch from tw wooo'
-//     .split(' ')
-//     .forEach(function (item) {
-//       s.push(item)
-//     })
-//   s.push(null)
-//   t.plan(17)
-//   s.pipe(sis.classify({
-//     maxNGramLength: 3
-//   }))
-//     .on('data', function (data) {
-//       t.looseEquals(data, expectedResults.shift())
-//     })
-//     .on('error', function (err) {
-//       t.error(err)
-//     })
-//     .on('end', function () {
-//       t.equals(expectedResults.length, 0)
-//     })
-// })
+test('classify maxNGramLength is 3', function (t) {
+  var expectedResults = [
+    { token: 'endlessly', documents: [ '10' ] },
+    { token: 'endlessly sophisticated', documents: [ '10' ] },
+    { token: 'sophisticated', documents: [ '10' ] },
+    { token: 'this', documents: [ '10', '2', '7' ] },
+    { token: 'is', documents: [ '4', '8' ] },
+    { token: 'is a', documents: [ '4' ] },
+    { token: 'a', documents: [ '4', '6', '7' ] },
+    { token: 'swiss', documents: [ '10', '2', '3', '4', '5', '9' ] },
+    { token: 'and', documents: [ '1', '10', '7' ] },
+    { token: 'a', documents: [ '4', '6', '7' ] },
+    { token: 'watch', documents: [ '1', '10', '2', '3', '7', '9' ] },
+    { token: 'watch from', documents: [ '7' ] },
+    { token: 'watch from tw', documents: [ '7' ] },
+    { token: 'from', documents: [ '5', '6', '7', '8' ] },
+    { token: 'from tw', documents: [ '7' ] },
+    { token: 'tw', documents: [ '7' ] }
+  ]
+  var s = new Readable()
+  'Endlessly sophisticated This is a really interesting sentence about swiss watches and also a watch from tw wooo'
+    .split(' ')
+    .forEach(function (item) {
+      s.push(item)
+    })
+  s.push(null)
+  t.plan(17)
+  s.pipe(sis.classify({
+    maxNGramLength: 3
+  }))
+    .on('data', function (data) {
+      t.looseEquals(data, expectedResults.shift())
+    })
+    .on('error', function (err) {
+      t.error(err)
+    })
+    .on('end', function () {
+      t.equals(expectedResults.length, 0)
+    })
+})
 
-// test('classify on name field', function (t) {
-//   var expectedResults = [
-//     { token: 'swiss', documents: [ '10', '2', '3', '4', '5' ] },
-//     { token: 'swiss army', documents: [ '2' ] },
-//     { token: 'army', documents: [ '2' ] },
-//     { token: 'watch', documents: [ '1' ] },
-//     { token: 'tw', documents: [ '7' ] }
-//   ]
-//   var s = new Readable()
-//   'Endlessly sophisticated This is a really interesting sentence about swiss army watches and also a watch from tw wooo'
-//     .split(' ')
-//     .forEach(function (item) {
-//       s.push(item)
-//     })
-//   s.push(null)
-//   t.plan(6)
-//   s.pipe(sis.classify({
-//     maxNGramLength: 5,
-//     field: 'name'
-//   }))
-//     .on('data', function (data) {
-//       t.looseEquals(data, expectedResults.shift())
-//     })
-//     .on('error', function (err) {
-//       t.error(err)
-//     })
-//     .on('end', function () {
-//       t.equals(expectedResults.length, 0)
-//     })
-// })
+test('classify on name field', function (t) {
+  var expectedResults = [
+    { token: 'swiss', documents: [ '10', '2', '3', '4', '5' ] },
+    { token: 'swiss army', documents: [ '2' ] },
+    { token: 'army', documents: [ '2' ] },
+    { token: 'watch', documents: [ '1' ] },
+    { token: 'tw', documents: [ '7' ] }
+  ]
+  var s = new Readable()
+  'Endlessly sophisticated This is a really interesting sentence about swiss army watches and also a watch from tw wooo'
+    .split(' ')
+    .forEach(function (item) {
+      s.push(item)
+    })
+  s.push(null)
+  t.plan(6)
+  s.pipe(sis.classify({
+    maxNGramLength: 5,
+    field: 'name'
+  }))
+    .on('data', function (data) {
+      t.looseEquals(data, expectedResults.shift())
+    })
+    .on('error', function (err) {
+      t.error(err)
+    })
+    .on('end', function () {
+      t.equals(expectedResults.length, 0)
+    })
+})
 
-// test('classify on name field', function (t) {
-//   var expectedResults = [
-//     { token: 'swiss', documents: [ '10', '2', '3', '4', '5' ] },
-//     { token: 'swiss army', documents: [ '2' ] },
-//     { token: 'army', documents: [ '2' ] },
-//     { token: 'watch', documents: [ '1' ] },
-//     { token: 'tw', documents: [ '7' ] }
-//   ]
-//   var s = new Readable()
-//   'Endlessly sophisticated This is a really interesting sentence about swiss army watches and also a watch from tw wooo'
-//     .split(' ')
-//     .forEach(function (item) {
-//       s.push(item)
-//     })
-//   s.push(null)
-//   t.plan(6)
-//   s.pipe(sis.classify({
-//     maxNGramLength: 5,
-//     field: 'name'
-//   }))
-//     .on('data', function (data) {
-//       t.looseEquals(data, expectedResults.shift())
-//     })
-//     .on('error', function (err) {
-//       t.error(err)
-//     })
-//     .on('end', function () {
-//       t.equals(expectedResults.length, 0)
-//     })
-// })
+test('classify on name field', function (t) {
+  var expectedResults = [
+    { token: 'swiss', documents: [ '10', '2', '3', '4', '5' ] },
+    { token: 'swiss army', documents: [ '2' ] },
+    { token: 'army', documents: [ '2' ] },
+    { token: 'watch', documents: [ '1' ] },
+    { token: 'tw', documents: [ '7' ] }
+  ]
+  var s = new Readable()
+  'Endlessly sophisticated This is a really interesting sentence about swiss army watches and also a watch from tw wooo'
+    .split(' ')
+    .forEach(function (item) {
+      s.push(item)
+    })
+  s.push(null)
+  t.plan(6)
+  s.pipe(sis.classify({
+    maxNGramLength: 5,
+    field: 'name'
+  }))
+    .on('data', function (data) {
+      t.looseEquals(data, expectedResults.shift())
+    })
+    .on('error', function (err) {
+      t.error(err)
+    })
+    .on('end', function () {
+      t.equals(expectedResults.length, 0)
+    })
+})
 
-// test('classify on name field and return docs', function (t) {
-//   var expectedResults = [
-//     batch[0],
-//     batch[9],
-//     batch[1],
-//     batch[2],
-//     batch[3],
-//     batch[4],
-//     batch[6]
-//   ]
+test('classify on name field and return docs', function (t) {
+  var expectedResults = [
+    batch[0],
+    batch[9],
+    batch[1],
+    batch[2],
+    batch[3],
+    batch[4],
+    batch[6]
+  ]
 
-//   var s = new Readable()
-//   'Endlessly sophisticated This is a really interesting sentence about swiss army watches and also a watch from tw wooo'
-//     .split(' ')
-//     .forEach(function (item) {
-//       s.push(item)
-//     })
-//   s.push(null)
-//   t.plan(7)
+  var s = new Readable()
+  'Endlessly sophisticated This is a really interesting sentence about swiss army watches and also a watch from tw wooo'
+    .split(' ')
+    .forEach(function (item) {
+      s.push(item)
+    })
+  s.push(null)
+  t.plan(7)
 
-//   var ids = []
-//   s.pipe(sis.classify({
-//     maxNGramLength: 5,
-//     field: 'name'
-//   }))
-//     .on('data', function (data) {
-//       ids = ids.concat(data.documents)
-//     })
-//     .on('error', function (err) {
-//       t.error(err)
-//     })
-//     .on('end', function () {
-//       ids = ids.sort().filter(function (item, pos, ary) {
-//         return !pos || item !== ary[pos - 1]
-//       })
-//       sis.get(ids).on('data', function (data) {
-//         t.looseEqual(data, expectedResults.shift())
-//       })
-//     })
-// })
+  var ids = []
+  s.pipe(sis.classify({
+    maxNGramLength: 5,
+    field: 'name'
+  }))
+    .on('data', function (data) {
+      ids = ids.concat(data.documents)
+    })
+    .on('error', function (err) {
+      t.error(err)
+    })
+    .on('end', function () {
+      ids = ids.sort().filter(function (item, pos, ary) {
+        return !pos || item !== ary[pos - 1]
+      })
+      sis.get(ids).on('data', function (data) {
+        t.looseEqual(data, expectedResults.shift())
+      })
+    })
+})
